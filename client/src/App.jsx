c8287432df978aa3a83d9e0c998fb7a9f22cd369
BrunoTorresF <bruno.torres.fabila@gmail.com>
2020-04-13 13:47:22 -0500

    init pokedex repo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 // RHL only for front end development
 import { hot } from 'react-hot-loader';
 
-class App extends Component {
-  render() {
-    return <div>Welcome to a Modern Minimal React Boilerplate</div>;
-  }
-}
+const App = () => <div>Pokedex Homepage will go here</div>;
 // hot export works with RHL. Remove line 11 when starting fullstack integration
 export default hot(module)(App);
 // Uncomment line 13 & delete line 11 when starting fullstack integration
