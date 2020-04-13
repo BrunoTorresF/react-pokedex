e1084b7726dadcd1343de53745efe95a0bd32838
Bruno Torres <33846976+BrunoTorresF@users.noreply.github.com>
2020-04-13 13:25:24 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+/* eslint global-require: 0 */
+
+const path = require('path');
+
+module.exports = {
+  entry: ['./client/src/Index.js'],
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'client/dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: ['babel-loader'],
+        exclude: /node_modules|packages/,
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: ['eslint-loader'],
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: 'style-loader',
+            options: {
+              hmr: true,
+            },
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [
+                require('autoprefixer')({
+                  browsers: ['> 1%'],
+                }),
+              ],
+            },
+          },
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+
+  devServer: {
+    contentBase: './client/dist',
+    host: 'localhost',
+    historyApiFallback: true,
+    // respond to 404s with index.html
+    inline: true,
+  },
+};
